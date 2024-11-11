2b57a3ca02bf80ec29c8b431eabc5eccbac8348c
MrCarrot102 <m.sierakowski@op.pl>
2024-11-11 12:54:52 +0100

    poczatek listy 2

--- Lista 2/app/src/main/java/com/example/lista2/FragmentB.java
+++ Lista 2/app/src/main/java/com/example/lista2/FragmentB.java
@@ -1,27 +1,14 @@
 package com.example.lista2;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.example.lista2.databinding.FragmentBlankBinding;
+public class FragmentB {
+    private FragmentBBinding binding;
 
-
-public class FragmentB extends Fragment {
-    public FragmentB() {
-        // Required empty public constructor
-    }
-    private FragmentB binding;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        binding = FragmentB.inflate(getLayoutInflater());
-        // Inflate the layout for this fragment
+    public View onCreateView (@NonNull LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState){
+        binding = FragmentBBinding.inflate(inflater);
         return binding.getRoot();
     }
 }
