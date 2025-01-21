499687abf18e4f83a25a98c083a9a937c28e9e35
Mateusz Thinkpad <mateusz@example.com>
2025-01-21 10:56:46 +0100

    Dodanie grafiki pocisku

--- Maslolot/app/src/main/java/com/example/maslolot/ui/components/EnemyComposable.kt
+++ Maslolot/app/src/main/java/com/example/maslolot/ui/components/EnemyComposable.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.maslolot.R
