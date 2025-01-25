6fac02208a66e85717ad56078bea3c4dbf6184b5
MrCarrot102 <m.sierakowski@op.pl>
2025-01-25 21:59:54 +0100

    Naprawienie błedu z tabelą wyników oraz dodanie tła menu

--- Maslolot/app/src/main/java/com/example/maslolot/ui/HighScoresScreen.kt
+++ Maslolot/app/src/main/java/com/example/maslolot/ui/HighScoresScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
@@ -17,7 +18,7 @@ import com.example.maslolot.utils.HighScoresManager
 
 @Composable
 fun HighScoresScreen(context: Context) {
-    val highScores = mutableStateListOf<Pair<String,Int>>()
+    val highScores = remember { mutableStateListOf<Pair<String,Int>>() }
     LaunchedEffect(Unit) {
         val loadedScores = HighScoresManager.loadScores(context)
         highScores.clear()
--- Maslolot/app/src/main/java/com/example/maslolot/ui/StartScreen.kt
+++ Maslolot/app/src/main/java/com/example/maslolot/ui/StartScreen.kt
@@ -1,5 +1,7 @@
 package com.example.maslolot.ui
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -7,20 +9,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
+import com.example.maslolot.R
 
 @Composable
 fun StartScreen(navController: NavHostController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment =  Alignment.CenterHorizontally
-    ) {
-        Button(onClick = { navController.navigate("game_screen")}){
-            Text(text = "Start Game")
-        }
-        Button(onClick = { navController.navigate("high_scores")}){
-            Text(text = "High Scores")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.menu),
+            contentDescription = "Menu",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+            Button(onClick = { navController.navigate("game_screen") }) {
+                Text(text = "Start Game")
+            }
+            Button(onClick = { navController.navigate("high_scores") }) {
+                Text(text = "High Scores")
+            }
         }
     }
 }
